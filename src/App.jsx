20885f6fd58033adcb4a374b5922351f144136e1
PhatJack <tienphatng.693@gmail.com>
2023-07-26 08:46:28 +0700

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,106 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import bgGradient from './assets/bg.svg'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { MdOutlineMyLocation } from 'react-icons/md'
-import { Lightning, Sunny, Umbrella } from './assets'
+import { Humidity, Lightning, Umbrella, Wind, Night, Sunny, CloudyandRainy, Clouds, Clear } from './assets'
+import axios from 'axios'
+import NotFound from './assets/404.png'
 function App() {
 
+	const [data, setData] = useState({})
+	const [location, setLocation] = useState('')
+
+	const currentDate = new Date();
+	const formattedDate = currentDate.toLocaleDateString("en-US", {
+		weekday: "short",
+		month: "short",
+		day: "numeric",
+	});
+
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=1ead008b6f7bc918321c3c7cca663b0c`
+
+	// console.log(url)
+
+	useEffect(() => {
+		const timeOutId = setTimeout(() => setLocation(location), 500);
+		return () => clearTimeout(timeOutId);
+	}, [location]);
+
+	const getWeather = (event) => {
+		if (event.key == 'Enter') {
+			axios.get(url).then(response => {
+				setData(response.data)
+				console.log(response.data)
+			})
+		}
+	}
+	console.log(data)
 	return (
 		<div className="flex justify-center items-center h-screen w-full bg-gradient-to-br from-[#FEE3BC] to-[#76e0f3] relative">
 			<img src={bgGradient} alt="" className='absolute w-full h-full object-cover' />
 			<div className='w-[400px] h-[750px] bg-gray-300 rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border-[8px] border-gray-100'>
 				<div className="px-10 py-5 w-full h-full">
 					<div className="border-[2px] text-white border-white rounded-full flex items-center justify-between">
-						<input type="text" placeholder='Enter your location' className='focus:outline-none w-[300px] text-lg p-3 bg-transparent placeholder:text-white' />
+						<input
+							type="text"
+							placeholder='Enter your location'
+							value={location}
+							onChange={event => setLocation(event.target.value)}
+							onKeyPress={getWeather}
+							className='focus:outline-none w-[300px] text-lg p-3 bg-transparent placeholder:text-white' />
 						<AiOutlineSearch size={30} color='white' className='mr-4 cursor-pointer' />
 					</div>
-					<div className="top-content mt-10 text-white">
-						<div className="left-content my-6">
-							<section className='flex flex-col mb-5'>
-								<p className="city text-4xl text-[#313341]">Stockholm,
-									Sweden</p>
-								<p className='text-[#9A938C] mt-2'>Tue, Jun 30</p>
-							</section>
-							<div className="flex justify-between items-center">
-								<div className="w-44 h-44">
-									<img src={Sunny} alt="" className='w-full h-full object-contain' />
+					{
+						Object.keys(data).length !== 0 ?
+							<div className="">
+								<div className="top-content mt-10 text-white">
+									<div className="left-content my-6">
+										<section className='flex flex-col mb-5'>
+											<p className="city text-4xl text-[#313341]">{data ? data.name : null}</p>
+											<p className='text-[#9A938C] mt-2'>{formattedDate}</p>
+										</section>
+										<div className="flex justify-between items-center">
+											<div className="w-44 h-44">
+												<img src={Sunny} alt="" className='w-full h-full object-contain' />
+											</div>
+											<div className="">
+												<p className="temperature text-6xl text-[#303345] font-bold">
+													{data.main ? (data.main.temp - 273.15).toFixed(0) + '°C' : ''}
+												</p>
+												<p className="weather-description text-md text-[#303345]">{data.weather ? data.weather[0].main : 'undefined'}</p>
+											</div>
+										</div>
+									</div>
 								</div>
-								<div className="">
-									<p className="temperature text-6xl text-[#303345]">
-										15&deg;C
-									</p>
-									<p className="weather-description text-md text-[#303345]">Cloudy</p>
+								<div className="bottom-content flex flex-col gap-4">
+									<div className="bg-[rgba(255,255,255,0.36)] w-full px-5 py-3 rounded-[20px] flex justify-between items-center">
+										<div className="flex items-center gap-3">
+											<img src={Umbrella} alt="" className='w-12 h-12 filter drop-shadow-lg bg-white rounded-2xl p-2' />
+											<p>Rainfall</p>
+										</div>
+										<span className='text-sm text-gray-600'>{data.main ? data.main.pressure : 'undefined'}</span>
+									</div>
+									<div className="bg-[rgba(255,255,255,0.36)] w-full px-5 py-3 rounded-[20px] flex justify-between items-center">
+										<div className="flex items-center gap-3">
+											<img src={Wind} alt="" className='w-12 h-12 filter drop-shadow-lg bg-white rounded-2xl p-2' />
+											<p>Wind</p>
+										</div>
+										<span className='text-sm text-gray-600'>{data.wind ? data.wind.speed + 'km/h' : 'undefined'}</span>
+									</div>
+									<div className="bg-[rgba(255,255,255,0.36)] w-full px-5 py-3 rounded-[20px] flex justify-between items-center">
+										<div className="flex items-center gap-3">
+											<img src={Humidity} alt="" className='w-12 h-12 filter drop-shadow-lg bg-white rounded-2xl p-2' />
+											<p>Humidity</p>
+										</div>
+										<span className='text-sm text-gray-600'>{data.main ? data.main.humidity + '%' : 'undefined'}</span>
+									</div>
 								</div>
+							</div> : <div className="flex justify-center items-center w-full h-full box-border">
+								<img src={NotFound} alt="" className='w-full object-contain' />
 							</div>
-						</div>
-					</div>
-					<div className="bottom-content">
-						<div className="bg-[rgba(255,255,255,0.36)] w-full px-5 py-3 rounded-[25px]">
-							<img src={Umbrella} alt="" className='w-16 h-16 filter drop-shadow-lg bg-white rounded-2xl p-2' />
-						</div>
-					</div>
+					}
 				</div>
 			</div>
 		</div>
--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,10 +1,11 @@
 import Lightning from './lightning.svg'
-import Night from './night.svg'
-import Sunny from './sunny.svg'
-import Warm from './warm.svg'
-import SunnyandRainy from './sunnyandrainy.svg'
+import Night from './Night.svg'
+import Sunny from './Sunny.svg'
+import Clouds from './Clouds.svg'
+import CloudyandRainy from './CloudyandRain.svg'
 import Humidity from './humidity.svg'
 import Umbrella from './umbrella.svg'
 import Wind from './wind.svg'
+import Clear from './Clear.svg'
 
-export { Lightning, Night, Sunny, SunnyandRainy, Warm, Wind, Humidity, Umbrella }
+export { Lightning, Night, Sunny, CloudyandRainy, Clouds, Wind, Humidity, Umbrella,Clear }
